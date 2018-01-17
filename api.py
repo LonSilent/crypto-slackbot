48dbfdfb8bb49b89e0bf51fa96daf0d1fc80aa3c
LonSilent <101703044@nccu.edu.tw>
2018-01-17 12:02:43 +0800

    add percentage

--- api.py
+++ api.py
@@ -4,7 +4,12 @@ import timeit
 
 # normalize string to make float variable
 def remove_punc(str):
-    return str.replace('$', '').replace(',', '')
+    return str.replace('$', '').replace(',', '').strip()
+
+def add_plus(pct):
+    if '-' not in pct:
+        pct = '+' + pct
+    return '(' + pct + '%)'
 
 # get price by symbol
 def get_price(symbol):
@@ -19,6 +24,24 @@ def get_price(symbol):
         result[sym] = price
 
     return result
+
+def get_price_full(symbol):
+    result = {}
+    info = {}
+    coin_data = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms={}&tsyms=USD,BTC,ETH'.format(symbol)).json()
+
+    for sym, data in coin_data['RAW'][symbol].items():
+        result[sym] = data['PRICE']
+
+    usd_pct = coin_data['DISPLAY'][symbol]['USD']['CHANGEPCT24HOUR']
+    btc_pct = coin_data['DISPLAY'][symbol]['BTC']['CHANGEPCT24HOUR']
+    eth_pct = coin_data['DISPLAY'][symbol]['ETH']['CHANGEPCT24HOUR']
+
+    usd_pct = add_plus(usd_pct)
+    btc_pct = add_plus(btc_pct)
+    eth_pct = add_plus(eth_pct)
+
+    return result, usd_pct, btc_pct, eth_pct
 
 # get maicoin price, only support BTC, ETH, LTC
 def get_maicoin_price(symbol, market_price):
@@ -42,15 +65,16 @@ def get_maicoin_price(symbol, market_price):
 
 def collect_data(symbol):
     symbol = symbol.upper()    
-    price_data = get_price(symbol)
+    # price_data = get_price(symbol)
+    price_data, usd_pct, btc_pct, eth_pct = get_price_full(symbol)
     if price_data == None:
         return 'There is no data for the coin {}'.format(symbol)
 
     # format return message    
     result_message = symbol + '\n' + \
-        '[USD] {}\n'.format(price_data['USD']) + \
-        '[BTC] {}\n'.format(price_data['BTC']) + \
-        '[ETH] {}\n'.format(price_data['ETH'])
+        '[USD] {}'.format(price_data['USD']) + ' ' + usd_pct + \
+        '\n[BTC] {}'.format(price_data['BTC']) + ' ' + btc_pct + \
+        '\n[ETH] {}'.format(price_data['ETH']) + ' ' + eth_pct + '\n'
 
     if symbol in ['BTC', 'ETH', 'LTC']:
         maicoin_price = get_maicoin_price(symbol, price_data['USD'])
@@ -62,11 +86,14 @@ def collect_data(symbol):
 
 if __name__ == '__main__':
     start = timeit.default_timer()
+
+    # print(get_price_full('BTC'))
     
-    print(collect_data('btc'))
-    print(collect_data('eth'))
-    print(collect_data('ltc'))
-    print(collect_data('ada'))
+    # print(collect_data('btc'))
+    # print(collect_data('eth'))
+    # print(collect_data('ltc'))
+    print(collect_data('xrp'))
+    # print(collect_data('ada'))
     
     end = timeit.default_timer()
     print("spend {} secs".format(end-start))
